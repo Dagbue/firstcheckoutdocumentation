5e23bd15a3d98697fd4e6e6d07ecef4e36b17e5d
Dagbue <100795340+Dagbue@users.noreply.github.com>
2025-09-15 18:12:36 +0100

    Fix Link Component Import Error

--- src/components/sections/ApiKeysSection.tsx
+++ src/components/sections/ApiKeysSection.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Key, Shield, Zap, AlertTriangle, Lock, Database, Settings, Monitor } from 'lucide-react';
 import { CodeBlock } from '../CodeBlock';
 import { apiKeyDashImage } from "../../assets";
